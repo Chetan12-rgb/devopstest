e3424f1480879a5bab07993a751fec0f40e7e559
Chetan12-rgb <chetan948y@gmail.com>
2024-06-25 16:51:46 +0000

    Initial commit of testpython.py

---
+++ testpython.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+#hi how are you
+
+#hello
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainpage():
+        return "Hello from my python server"
+
+if(__name__=='__main__'):
+        app.run(host='0.0.0.0',debug=True)
